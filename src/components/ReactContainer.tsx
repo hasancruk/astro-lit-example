f845a1537b3a1ef9e92a2a4f9e7ce5a2151ff1ee
Hasan Ali <muhammadhasan.ali@cancer.org.uk>
2023-09-08 19:00:54 +0100

    Remove debug container react component

--- src/components/ReactContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactButton } from "./ReactButton";
-
-export const Container = () => (
-  <ReactButton variant="secondary" onClick={() => console.log("React button clicked")}>React click me</ReactButton>
-);
-
-export default Container;
